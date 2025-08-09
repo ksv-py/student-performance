4f44b3ebfdb19376c41b5a4ed01ab98be3ddea96
ksv-py <keshavjangid301@gmail.com>
2025-08-09 16:27:40 +0530

    vercel deployment

--- api/index.py
+++
@@ -1,8 +0,0 @@
-# api/index.py
-from app import app  # Import your Flask instance
-from werkzeug.middleware.dispatcher import DispatcherMiddleware
-from werkzeug.wrappers import Response
-
-# Vercel expects a function called 'handler'
-def handler(environ, start_response):
-    return app.wsgi_app(environ, start_response)
